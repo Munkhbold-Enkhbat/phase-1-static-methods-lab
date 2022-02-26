d44172e49f3936126b1ed75ca0cb3a8c8de452e7
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-02-25 16:22:27 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str = str[0].toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str = str.replace(/[^a-zA-Z0-9-'\s]+/g, '')
+  }
+
+  static titleize(str) {
+    let nonCaptWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let arr = str.split(' ')
+   
+    let titleArr = arr.map((word, index) => {
+      if(index === 0 || !nonCaptWords.includes(word)) {        
+        return this.capitalize(word)       
+      } else {
+        return word
+      }
+    })  
+    let word = titleArr.join(' ')
+    return word
+  }
 }
